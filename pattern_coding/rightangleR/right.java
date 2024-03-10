47e352037526778936e1e63d475bb43bb75c70f6
java_learing <aswinsandeep4@gmail.com>
2024-03-10 16:00:36 +0530

    files

---
+++ pattern_coding/rightangleR/right.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class InOut{
+    public void outPutFirstLine(String data){
+        System.out.print(data);
+    }
+    public void outPutNextLine(String data){
+        System.out.println(data);
+    }
+    public int inPutInt(Scanner sc){
+       return sc.nextInt();
+    }
+    public String inPutString(Scanner sc){
+        return sc.nextLine();
+    }
+}
+class RightLogic{
+    public void logic(int rowLength,InOut io){
+        int star=rowLength;
+        for(int rows=0;rows<=rowLength;rows++){
+            for(int stars=0;stars<=star;stars++){
+                io.outPutFirstLine("*");
+                io.outPutFirstLine(" ");
+            }
+            star--;
+            io.outPutNextLine("");
+        }
+    }
+}
+class MainRight{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+    InOut io=new InOut();
+    RightLogic rl=new RightLogic();
+    int rows;
+    try{
+        io.outPutNextLine("Enter Number of Rows");
+        rows=io.inPutInt(sc);
+        rl.logic(rows,io);
+    }catch(Exception e){
+        io.outPutFirstLine(e.toString());
+    }finally{
+        sc.close();
+    }
+    }
+    
+}
